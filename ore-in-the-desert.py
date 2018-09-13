da1be394c86c5f1eef3c19d6d4692752211b1ebd
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2018-09-13 21:21:35 +0500

    searching ferrum not implemented

---
+++ ore-in-the-desert.py
@@ -0,0 +1,23 @@
+def checkio(previous):
+    return [0, 0]
+
+
+if __name__ == '__main__':
+    #This part is using only for self-testing.
+    def check_solution(func, ore):
+        recent_data = []
+        for step in range(4):
+            row, col = func([d[:] for d in recent_data])  # copy the list
+            if row < 0 or row > 9 or col < 0 or col > 9:
+                print("Where is our probe?")
+                return False
+            if (row, col) == ore:
+                return True
+            dist = round(((row - ore[0]) ** 2 + (col - ore[1]) ** 2) ** 0.5)
+            recent_data.append([row, col, dist])
+        print("It was the last probe.")
+        return False
+
+    assert check_solution(checkio, (1, 1)), "Example"
+    assert check_solution(checkio, (9, 9)), "Bottom right"
+    assert check_solution(checkio, (6, 6)), "Center"
